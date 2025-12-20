a8c0c2c09747428c1c8dc2edcfa687586f011a35
Priyanshu-Singh-25 <priyanshusinghh18@gmail.com>
2025-12-21 01:00:14 +0530

    Program to find largest among three integers
    Implemented a program to find the largest of three integers using if-else statements.

---
+++ If-Else_LargestAmongThreeNumbers.c
@@ -0,0 +1,52 @@
+//Problem
+
+      /*Write a program to determine the largest number among three integer numbers.
+      
+      Input Format
+      
+      The input consists of three integers a, b, and c, separated by spaces.
+      Constraints
+      
+      −10^3 ≤ a, b, c ≤10^3
+      
+      Output Format
+      
+      Print the largest number among the three integers along with the given message.
+      Sample Input 0
+      
+      5 10 15
+      Sample Output 0
+      
+      The largest number is : 15
+      Sample Input 1
+      
+      88 76 76
+      Sample Output 1
+      
+      The largest number is : 88
+      */
+
+//Solution
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>b && b>c){
+        printf("The largest number is : %d", a);
+    }
+    else if(a>b && b==c){
+        printf("The largest number is : %d", a);
+    }
+    else if(a==b && b<c){
+        printf("The largest number is : %d", c);
+    }
+    else if(a<b && b>c){
+        printf("The largest number is : %d", b);
+    }
+    else{
+        printf("The largest number is : %d", c);
+    }
+    
+    
+    return 0;
+}
