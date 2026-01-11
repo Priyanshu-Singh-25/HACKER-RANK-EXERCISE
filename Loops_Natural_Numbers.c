e2a01ff0dc2a43ca925779ce4ac37146e60c29c6
Priyanshu-Singh-25 <priyanshusinghh18@gmail.com>
2026-01-11 23:55:12 +0530

    Natural number printing from 1 to N
    The program takes a positive integer N as input and prints all natural numbers from 1 to N, each followed by a space.

---
+++ Loops_Natural_Numbers.c
@@ -0,0 +1,44 @@
+//Problem
+
+      /*Write a program that takes a positive integer N as input from the user and prints all natural numbers from 1 to N, with each number followed by a space (including the last number).
+      
+      Input Format
+      
+      A single positive integer, N, representing the range limit.
+      
+      Constraints
+      
+      0 <= N <= 10^6
+      
+      Output Format
+      
+      Print all natural numbers from 1 to N, separated by a space.
+      
+      Sample Input 0
+      
+      5
+      Sample Output 0
+      
+      1 2 3 4 5 
+      Sample Input 1
+      
+      10
+      Sample Output 1
+      
+      1 2 3 4 5 6 7 8 9 10 
+        */
+
+//Solution
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i; i<=n; ++i){
+        if(i>0)
+        printf("%d ", i);
+    } 
+
+    return 0;
+}
