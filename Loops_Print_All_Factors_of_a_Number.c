21eb154958693e9e118245c6af120f00051f7f2c
Priyanshu-Singh-25 <priyanshusinghh18@gmail.com>
2026-01-22 23:39:46 +0530

    Program to print factors of a number
    Implement a program to print all factors of a given positive integer N in ascending order.

---
+++ Loops_Print_All_Factors_of_a_Number.c
@@ -0,0 +1,51 @@
+//Problem
+
+/*Given a positive integer N, print all its factors in ascending order.
+
+You need to write a program that reads N and prints all its factors separated by spaces.
+
+Input Format
+
+A single line containing the integer N.
+
+Constraints
+
+1 ≤ 𝑁 ≤ 10^9
+Output Format
+
+Print all factors of N in ascending order, separated by a single space.
+
+Sample Input 0
+
+6
+Sample Output 0
+
+1 2 3 6
+Sample Input 1
+
+1
+Sample Output 1
+
+1
+Sample Input 2
+
+25
+Sample Output 2
+
+1 5 25
+*/
+
+//Solution
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; ++i){
+        if(n%i==0){
+            printf("%d ", i);
+        }
+        continue;
+    }
+  
+    return 0;
+}
